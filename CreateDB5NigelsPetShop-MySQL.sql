25def1e0f794d6639be5554903dad989affae684
Anne Wagner <44533995+amwagner19@users.noreply.github.com>
2021-05-02 14:00:13 -0700

    Update CreateDB5NigelsPetShop-MySQL.sql

--- CreateDB5NigelsPetShop-MySQL.sql
+++ CreateDB5NigelsPetShop-MySQL.sql
@@ -1,12 +1,13 @@
-DROP TABLE IF EXISTS Adoption;  
+
+	 
 CREATE TABLE Animal
 	 (AnimalID int,
 	  AnimalName varchar(20),
 	  Type varchar(3),
 	  Gender varchar(1),
 	  Breed varchar(20),
-	  Neutered varchar(1),
-	  Decalwed varchar(1),
+	  Neutered tinyint(1),
+	  Decalwed tinyint(1),
 	  CONSTRAINT Person_PK PRIMARY KEY (AnimalID));
 					    
 CREATE TABLE Adoption
@@ -23,12 +24,6 @@ CREATE TABLE Adoption
 		CONSTRAINT Adoption_FK3 FOREIGN KEY (AdoptionID)
 			REFERENCES Animal(AnimalID)); 
 			
-
-INSERT INTO Person (PersonID, Username, Password, Name)
-VALUES (12345, 'joe', 'schmo', 'Joe Schmo'); 
-
-INSERT INTO Person (PersonID, Username, Password, Name)
-VALUES (44444, 'stevebuscemi', 'dog', 'Steve Buscemi'); 
 
 					  
 CREATE TABLE Inventory
@@ -62,42 +57,4 @@ CREATE TABLE AnimalImmunization
 		 CONSTRAINT AnimalImmunization_FK1 FOREIGN KEY (ImmunizationID)
 			REFERENCES Immunization(ImmunizationID),
          	 CONSTRAINT AnimalImmunization_FK2 FOREIGN KEY (AnimalID)
-			REFERENCES Animal(AnimalID));
-						       
-INSERT INTO Person(PersonID, Username, Password, Name)
-VALUES ( 3, 'joe', 'schmo', 'joe schmo');
-										 
-INSERT INTO Inventory
-VALUES ( 1, 'Green Grass Kibble', 40.00);
-
-INSERT INTO Inventory
-VALUES ( 2, 'Indestructible Squeaky Toy', 25.00);
-INSERT INTO Inventory
-VALUES ( 3, 'Princess Leash', 20.00);
-
-INSERT INTO Inventory
-VALUES ( 4, 'Custom Collar', 15.00);
-
-INSERT INTO Order1
-VALUES (1, 2021-04-07, 3);
-	
-INSERT INTO Order1
-VALUES (2, 2021-04-04, 3);
-										 
-INSERT INTO OrderLine
-VALUES (3, 1, 2);
-										 
-INSERT INTO OrderLine
-VALUES (4, 2, 2);										
-										 
-INSERT INTO OrderLine
-VALUES (5, 1, 4);
-										 
-INSERT INTO OrderLine
-VALUES (5, 1, 5);																				 
-										 
-INSERT INTO OrderLine
-VALUES (5, 1, 6);
-										 
-INSERT INTO OrderLine
-VALUES (5, 1, 9);									
+			REFERENCES Animal(AnimalID));																	
