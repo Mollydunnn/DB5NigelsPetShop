dab4ff1cf70b717f35c90415ff2d75fd08b8827f
Anne Wagner <44533995+amwagner19@users.noreply.github.com>
2021-05-21 12:30:09 -0700

    Update __init__.py

--- DB5-Final-Project/DB5-Final-Project/flaskDemo/__init__.py
+++ DB5-Final-Project/DB5-Final-Project/flaskDemo/__init__.py
@@ -6,21 +6,9 @@ import mysql.connector as mysql
 from sqlalchemy import create_engine
 
 ''' Connect to MySQL database '''
-#try:
-#   conn = mysql.connector.connect(host='45.55.59.121',
-#                                  database='DB5',      
- #                                  user='DB5',
- #                                  password='studentDB5')
- 
-   
-#finally:
-#   conn.close()
-#    print ("done")
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://DB5:studentDB5@45.55.59.121/DB5'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
